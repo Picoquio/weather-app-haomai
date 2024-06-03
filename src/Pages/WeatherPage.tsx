ef3d0558d1f1ef94ccd72063f9d929db34cb8a70
Picoquio <84530694+Picoquio@users.noreply.github.com>
2024-06-02 22:48:57 -0300

    comienzos weather page

--- src/Pages/WeatherPage.tsx
+++ src/Pages/WeatherPage.tsx
@@ -1,17 +1,92 @@
-import { useContext } from "react"
+import { useContext, useEffect, useState } from "react"
 import { WeatherContext } from "../context/WeatherContext"
+import { WeatherAPIResponse } from "../api/interfaces";
+
+enum WeatherThemes {
+  RAINNING = 'Rainning',
+  CLEAR = 'Clear',
+  CLOUDY = 'Cloudy',
+  SNOWING = 'Snowing'
+}
 
 export const WeatherPage = () => {
-  const { weather, setWeather } = useContext(WeatherContext);
+  const { weather } = useContext(WeatherContext);
+  const [theme, setTheme] = useState<WeatherThemes | null>(null);
 
+  useEffect(() => {
+    setAppropiateTheme();
+  }, [])
+  
+
+  const setAppropiateTheme = () => {
+    const weatherCode: number = weather.weather[0].id;
+
+    if (weatherCode >= 200 && weatherCode <= 531) {
+      setTheme(WeatherThemes.RAINNING)
+    }
+    else if (weatherCode >= 600 && weatherCode <= 622) {
+      setTheme(WeatherThemes.SNOWING)
+    }
+    else if (weatherCode === 800) {
+      setTheme(WeatherThemes.CLEAR)
+    }
+    else if (weatherCode >= 801 && weatherCode <= 804) {
+      setTheme(WeatherThemes.SNOWING)
+    }
+  }
 
   return (
     <>
-      <div>WeatherPage</div>
+      <div>WeatherPage. El theme es: {theme}</div>
       <pre>
-        {JSON.stringify(weather, null, 3)}
+        {JSON.stringify(weather.weather[0].id, null, 3)}
       </pre>
     </>
 
   )
 }
+
+// {
+//   "coord": {
+//      "lon": -2,
+//      "lat": 33
+//   },
+//   "weather": [
+//      {
+//         "id": 804,
+//         "main": "Clouds",
+//         "description": "overcast clouds",
+//         "icon": "04n"
+//      }
+//   ],
+//   "base": "stations",
+//   "main": {
+//      "temp": 18.62,
+//      "feels_like": 18.03,
+//      "temp_min": 18.62,
+//      "temp_max": 18.62,
+//      "pressure": 1015,
+//      "humidity": 57,
+//      "sea_level": 1015,
+//      "grnd_level": 863
+//   },
+//   "visibility": 10000,
+//   "wind": {
+//      "speed": 4.56,
+//      "deg": 24,
+//      "gust": 7.48
+//   },
+//   "clouds": {
+//      "all": 99
+//   },
+//   "dt": 1717377018,
+//   "sys": {
+//      "country": "MA",
+//      "sunrise": 1717390795,
+//      "sunset": 1717441970
+//   },
+//   "timezone": 3600,
+//   "id": 2555157,
+//   "name": "Bouarfa",
+//   "cod": 200
+// }
