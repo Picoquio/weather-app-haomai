b4484a3b00f57060a3abdf5c65c345d8125ba803
Picoquio <84530694+Picoquio@users.noreply.github.com>
2024-06-05 16:22:05 -0300

    removido código innecesario

--- src/components/Navbar.tsx
+++ src/components/Navbar.tsx
@@ -28,17 +28,6 @@ export const Navbar = () => {
 
                 </div>
             </div>
-
-            {/* <!-- Mobile menu, show/hide based on menu state. --> */}
-            <div className="sm:hidden" id="mobile-menu">
-                <div className="space-y-1 px-2 pb-3 pt-2">
-                    {/* <!-- Current: "bg-gray-900 text-white", Default: "text-gray-300 hover:bg-gray-700 hover:text-white" --> */}
-                    <a href="#" className="bg-gray-900 text-white block rounded-md px-3 py-2 text-base font-medium" aria-current="page">Dashboard</a>
-                    <a href="#" className="text-gray-300 hover:bg-gray-700 hover:text-white block rounded-md px-3 py-2 text-base font-medium">Team</a>
-                    <a href="#" className="text-gray-300 hover:bg-gray-700 hover:text-white block rounded-md px-3 py-2 text-base font-medium">Projects</a>
-                    <a href="#" className="text-gray-300 hover:bg-gray-700 hover:text-white block rounded-md px-3 py-2 text-base font-medium">Calendar</a>
-                </div>
-            </div>
         </nav>
     )
 }
